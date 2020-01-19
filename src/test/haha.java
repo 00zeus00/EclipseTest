319fd934e77e48fd437c4209a33a5cbe8a0b8f11
zeus <1141903985@qq.com>
2020-01-19 22:30:37 +0800

    hah

--- src/test/haha.java
+++ src/test/haha.java
@@ -2,8 +2,7 @@ package test;
 public class haha {
 	public static void main(String[] args)
 	{
-		System.out.println("right");
-		System.out.println("c.d.getName()");
+		System.out.println("left");
 	}
 }
 
