e2a68a7bc3f77e55fba7804d4e98766c6178eaa5
zeus <1141903985@qq.com>
2020-01-19 20:58:28 +0800

    test

---
+++ src/test/haha.java
@@ -0,0 +1,31 @@
+package test;
+public class haha {
+	private String name;
+	private int age;
+	public haha()
+	{
+		
+	}
+	public haha(int age,String name)
+	{
+		this.age=age;
+		this.name=name;
+	}
+	public  void  setAge(int age)
+	{
+		this.age=age;
+	}
+	public int getAge()
+	{
+		return this.age;
+	}
+	public  void  setName(String name)
+	{
+		this.name=name;
+	}
+	public String getName()
+	{
+		return this.name;
+	}
+}
+
